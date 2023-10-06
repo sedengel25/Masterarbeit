5f2d3a29fc2f839189af64225d998bae4df1ad04
sedengel25 <sbstdnglahoo.com>
2023-10-06 17:18:36 +0200

    chore: renaming

--- src/02_utils.R
+++ src/02_utils.R
@@ -5,15 +5,15 @@
 ################################################################################
 
 # Documentation: create_overview
-# Usage: create_overview(list, dt_table, dt_plot)
+# Usage: create_overview(list, dt_table, dt_plot, input_path)
 # Description: Reads all files and creates an overview 
-# Args/Options: list, dt_table, dt_plot
+# Args/Options: list, dt_table, dt_plot, input_path
 # Returns: datatable
 # Output: ...
-create_overview <- function(list, dt_table, dt_plot) {
+create_overview <- function(list, dt_table, dt_plot, input_path) {
 
   for(file in list){
-  	data <- paste0(raw_data_path,file) %>% fread()
+  	data <- paste0(input_path, file) %>% read_rds()
   	
   	min_date <- min(data$start_time) %>% as.Date
   	max_date <- max(data$start_time) %>% as.Date
@@ -26,20 +26,25 @@ create_overview <- function(list, dt_table, dt_plot) {
   	}
   	
   	timespan <- seq(from = min_date, to = max_date, by = "1 day") 
+  	
   	provider <- strsplit(file, split = "_")[[1]][1]
   	city <- strsplit(file, split = "_")[[1]][2]
-  	
+  	id_temp <- strsplit(file, split = "_")[[1]][3]
+  	id <- strsplit(id_temp, split = "\\.")[[1]][1] %>% as.integer()
+
+
   	dt_plot <- bind_rows(dt_plot, data.table(
   		date = timespan,
   		provider = provider
   	))
   	
-  	days <- difftime(max_starttime, min_starttime, units = "days") %>% as.numeric %>% round
+  	days <- difftime(max_starttime, min_starttime, units = "days") %>% as.numeric %>% round %>% as.integer
   	
   	
   	dt_table <- bind_rows(dt_table, data.table(
   		provider = provider,
   		city = city,
+  		id = id,
   		min_starttime = min_starttime,
   		max_starttime = max_starttime,
   		days = days
@@ -47,9 +52,7 @@ create_overview <- function(list, dt_table, dt_plot) {
 
   }
 	
-	id <- seq(from = 1, to = nrow(dt_table))
-	dt_table <- dt_table %>%
-								mutate(id = id)
+
 	list_of_dts <- list(dt_plot = dt_plot, dt_table = dt_table)
 	return(list_of_dts)
 }
