d8a90acf17d3f6575fdf7a335be51af91fe6b5ad
sedengel25 <sbstdnglahoo.com>
2023-10-06 14:31:52 +0200

    feat: add city to dt

--- src/01_utils.R
+++ src/01_utils.R
@@ -11,16 +11,22 @@
 # Returns: datatable
 # Output: ...
 combine_files_to_dt <- function(list, path){
+	
+	
 	list_of_dt <- lapply(list, function(x){
+		
 		provider <- strsplit(x, split = "_")[[1]][1]
+		city <- strsplit(x, split = "_")[[1]][2]
 		dt <- paste0(path,x) %>% fread()
 		dt %>% 
 			mutate(id = as.character(id), 
-						 provider = provider)
+						 provider = provider,
+						 city = city)
 	})
 	
 	list_of_non_empty_dt <- list_of_dt[sapply(list_of_dt, nrow) > 0]
 	combined_dts <- dplyr::bind_rows(list_of_non_empty_dt) 
 	return(combined_dts)
+	
 }
 
