d601ec0f6a42ecc1f9b200ee807224964b7dace8
sedengel25 <sbstdnglahoo.com>
2023-10-06 15:45:44 +0200

    chore: renaming

--- src/00_config.R
+++ src/00_config.R
@@ -15,9 +15,10 @@ library(here)
 # Paths
 raw_data_path <- here("data/raw//")
 processed_data_1_path <- here("data/processed/01//")
+processed_data_2_path <- here("data/processed/02//")
 
-
-dt_path <- paste0(processed_data_1_path,"dt.rds")
+dt_table_path <- paste0(processed_data_1_path,"dt_table.rds")
+dt_path <- paste0(processed_data_2_path,"dt.rds")
 
 
 # Configurations
