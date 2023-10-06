f80d34d3306019ef49e95ea43634edf44dccb1fd
sedengel25 <sbstdnglahoo.com>
2023-10-06 16:14:09 +0200

    chore: renaming of files

---
+++ 03_read_data.R
@@ -0,0 +1,27 @@
+################################################################################
+# Big Data Analytics in Transportation (TU Dresden)
+# Master Thesis
+# This file reads in the data and saves the combined data
+################################################################################
+source("./src/00_config.R")
+source("./src/03_utils.R")
+
+
+################################################################################
+# Read all csv-files of e-scooter mobility data and combine it into a datatable
+################################################################################
+list_of_files <- list.files(raw_data_path)
+
+# Choose n random files
+int_n <- 3
+int_random <- sample(1:length(list_of_files), int_n)
+
+
+dt <- combine_files_to_dt(list = list_of_files[int_random], 
+													path = raw_data_path) 
+
+################################################################################
+# Save data
+################################################################################
+write_rds(x = dt, file = dt_path)
+
