dc026d194c5a93f509b3662b5ec5a904bc0619e3
sedengel25 <sbstdnglahoo.com>
2023-10-06 15:52:36 +0200

    chore: renaming

--- src/00_config.R
+++ src/00_config.R
@@ -18,6 +18,7 @@ processed_data_1_path <- here("data/processed/01//")
 processed_data_2_path <- here("data/processed/02//")
 
 dt_table_path <- paste0(processed_data_1_path,"dt_table.rds")
+dt_plot_path <- paste0(processed_data_1_path,"dt_plot.png")
 dt_path <- paste0(processed_data_2_path,"dt.rds")
 
 
