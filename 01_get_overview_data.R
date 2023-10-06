4e4076cb83f7dabed9126d52bc02f5be22e62882
sedengel25 <sbstdnglahoo.com>
2023-10-06 15:53:41 +0200

    feat: extend feature

---
+++ 01_get_overview_data.R
@@ -0,0 +1,56 @@
+################################################################################
+# Big Data Analytics in Transportation (TU Dresden)
+# Master Thesis
+# This file creates an overview of the collected data
+################################################################################
+source("./src/00_config.R")
+source("./src/01_utils.R")
+
+
+list_of_files <- list.files(raw_data_path)
+
+# Create empty data.table
+dt_plot <- data.table(
+	date = as.Date(character(0)),
+	provider = character(0)
+)
+
+dt_table <- data.table(
+	provider = character(0),
+	city = character(0),
+	min_starttime = as.POSIXct(character(0)),
+	max_starttime = as.POSIXct(character(0)),
+	days = numeric(0)
+)
+
+
+list_of_dt <- create_overview(list = list_of_files, 
+													   dt_plot = dt_plot, 
+													   dt_table = dt_table)
+
+dt_plot <- list_of_dt$dt_plot
+
+dt_table <- list_of_dt$dt_table
+# Plot data --------------------------------------
+
+
+overview_data <- ggplot(dt_plot, aes(x = date, y = provider)) +
+	geom_point() +
+	scale_x_date(limits = c(min(dt_plot$date), max(dt_plot$date)),
+							 date_labels = "%d-%m-%Y") +
+	scale_y_discrete(limits = c("VOI", "BOLT", "TIER")) +
+	labs(x = "date", y = "provider") 
+
+ggsave(dt_plot_path, plot = overview_data, 
+			 width = 30, height = 20, units = "cm")
+
+
+# Save data --------------------------------------
+write_rds(x = dt_table, path = dt_table_path)
+
+
+
+
+
+
+
--- src/01_utils.R
+++ src/01_utils.R
@@ -1,32 +1,57 @@
 ################################################################################
 # Big Data Analytics in Transportation (TU Dresden)
 # Master Thesis
-# This file contains all functions to execute 01_read_data.R
+# This file contains all functions to execute 01_get_overview_data.R
 ################################################################################
 
-# Documentation: combine_files_to_dt
-# Usage: combine_files_to_dt(list)
-# Description: comines list of filenames into 1 datatable
-# Args/Options: list
+# Documentation: create_overview
+# Usage: create_overview(list, dt_table, dt_plot)
+# Description: Reads all files and creates an overview 
+# Args/Options: list, dt_table, dt_plot
 # Returns: datatable
 # Output: ...
-combine_files_to_dt <- function(list, path){
+create_overview <- function(list, dt_table, dt_plot) {
+
+  for(file in list){
+  	data <- paste0(raw_data_path,file) %>% fread()
+  	
+  	min_date <- min(data$start_time) %>% as.Date
+  	max_date <- max(data$start_time) %>% as.Date
+  	
+  	min_starttime <- min(data$start_time) %>% as.POSIXct
+  	max_starttime <- max(data$start_time) %>% as.POSIXct
+  	
+  	if(is.infinite(min_date)){
+  		next
+  	}
+  	
+  	timespan <- seq(from = min_date, to = max_date, by = "1 day") 
+  	provider <- strsplit(file, split = "_")[[1]][1]
+  	city <- strsplit(file, split = "_")[[1]][2]
+  	
+  	dt_plot <- bind_rows(dt_plot, data.table(
+  		date = timespan,
+  		provider = provider
+  	))
+  	
+  	days <- difftime(max_starttime, min_starttime, units = "days") %>% as.numeric %>% round
+  	
+  	
+  	dt_table <- bind_rows(dt_table, data.table(
+  		provider = provider,
+  		city = city,
+  		min_starttime = min_starttime,
+  		max_starttime = max_starttime,
+  		days = days
+  	))
+
+  }
 	
-	
-	list_of_dt <- lapply(list, function(x){
-		
-		provider <- strsplit(x, split = "_")[[1]][1]
-		city <- strsplit(x, split = "_")[[1]][2]
-		dt <- paste0(path,x) %>% fread()
-		dt %>% 
-			mutate(id = as.character(id), 
-						 provider = provider,
-						 city = city)
-	})
-	
-	list_of_non_empty_dt <- list_of_dt[sapply(list_of_dt, nrow) > 0]
-	combined_dts <- dplyr::bind_rows(list_of_non_empty_dt) 
-	return(combined_dts)
-	
+	id <- seq(from = 1, to = nrow(dt_table))
+	dt_table <- dt_table %>%
+								mutate(id = id)
+	list_of_dts <- list(dt_plot = dt_plot, dt_table = dt_table)
+	return(list_of_dts)
 }
+
 
