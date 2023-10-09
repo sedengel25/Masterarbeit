de2bbb37cb73e7dadcc41483042cb397645f7e5a
sedengel25 <sbstdnglahoo.com>
2023-10-09 14:12:02 +0200

    chore: playing around

--- 03_read_data.R
+++ 03_read_data.R
@@ -15,13 +15,14 @@ list_of_files <- list.files(processed_data_1_path)
 # Choose the files you want to read in, based on the overview
 dt_table <- read_rds(dt_table_path)
 
+
 list_of_ids <- choose_datasets(dt = dt_table,
 															 days = 14,
 															 provider = "BOLT",
 															 city = "BERLIN")
 
-
-dt <- combine_files_to_dt(list = list_of_files[list_of_ids], 
+list_of_ids
+dt <- combine_files_to_dt(list = list_of_files[list_of_ids[1]], 
 													path = processed_data_1_path) 
 
 
