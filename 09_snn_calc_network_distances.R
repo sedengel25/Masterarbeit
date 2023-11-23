eac65aaa6a52ae18cdb667a55b9dbdd1489c8615
sedengel25 <sbstdnglahoo.com>
2023-11-23 09:51:53 +0100

    feat: calc nd's done

--- 09_snn_calc_network_distances.R
+++ 09_snn_calc_network_distances.R
@@ -6,40 +6,41 @@
 source("./src/00_config.R")
 source("./src/00_utils.R")
 source("./src/00_config_psql.R")
+source("./src/09_utils.R")
 
-dt_network <- RPostgres::dbReadTable(con, "col_2po_4pgr_subset") %>%
+char_city_prefix <- "col"
+char_osm2po_subset <- paste0(char_city_prefix, "_2po_4pgr_subset")
+char_mapped_o_points <- paste0(char_city_prefix, "_mapped_o_points")
+char_mapped_d_points <- paste0(char_city_prefix, "_mapped_d_points")
+char_dist_mat <- paste0(char_city_prefix, "_dist_mat")
+
+dt_network <- RPostgres::dbReadTable(con, char_osm2po_subset) %>%
 	mutate(m = km*1000) %>%
 	select(id, source, target, m) 
 
 
-dt_mapped_o <- RPostgres::dbReadTable(con, "col_mapped_o_points") %>%
+dt_mapped_o <- RPostgres::dbReadTable(con, char_mapped_o_points) %>%
 	select(id, line_id, distance_to_start, distance_to_end)
 
 
 
-dt_mapped_d <- RPostgres::dbReadTable(con, "col_mapped_d_points") %>%
+dt_mapped_d <- RPostgres::dbReadTable(con, char_mapped_d_points) %>%
 	select(id, line_id, distance_to_start, distance_to_end)
 
 
-dt_dist_mat <- RPostgres::dbReadTable(con, "distance_matrix_reduced_wo_dup")
+# dt_dist_mat <- RPostgres::dbReadTable(con, char_dist_mat)
 
 
-query <- "SELECT m1.id as o_m, 
-m2.id as o_n,
-LEAST(
-	m1.distance_to_start + m2.distance_to_start + COALESCE(pi_pk.m, 0),
-	m1.distance_to_end + m2.distance_to_end + COALESCE(pj_pl.m, 0),
-	m1.distance_to_start + m2.distance_to_end + COALESCE(pi_pl.m, 0),
-	m1.distance_to_end + m2.distance_to_start + COALESCE(pj_pk.m, 0)
-) AS nd
-FROM 
-col_mapped_o_points m1
-CROSS JOIN col_mapped_o_points m2
-INNER JOIN col_2po_4pgr_subset e_ij ON m1.line_id = e_ij.id
-INNER JOIN col_2po_4pgr_subset e_kl ON m2.line_id  = e_kl.id
-INNER JOIN distance_matrix_reduced pi_pk ON pi_pk.source = LEAST(e_ij.source, e_kl.source) AND pi_pk.target = GREATEST(e_ij.source, e_kl.source)
-INNER JOIN distance_matrix_reduced pj_pl ON pj_pl.source = LEAST(e_ij.target, e_kl.target) AND pj_pl.target = GREATEST(e_ij.target, e_kl.target)
-INNER JOIN distance_matrix_reduced pi_pl ON pi_pl.source = LEAST(e_ij.source, e_kl.target) AND pi_pl.target = GREATEST(e_ij.source, e_kl.target)
-INNER JOIN distance_matrix_reduced pj_pk ON pj_pk.source = LEAST(e_ij.target, e_kl.source) AND pj_pk.target = GREATEST(e_ij.target, e_kl.source)
-where m1.id < m2.id;"
+## Calculate NDs of all Origin points
+# calc_nd(con = con, 
+# char_mapped_points = char_mapped_o_points, 
+# char_osm2po_subset = char_osm2po_subset, 
+# char_dist_mat = char_dist_mat)
+
+
+## Calculate NDs of all Destination points
+# calc_nd(con = con, 
+# char_mapped_points = char_mapped_d_points, 
+# char_osm2po_subset = char_osm2po_subset, 
+# char_dist_mat = char_dist_mat)
 
---
+++ src/09_utils.R
@@ -0,0 +1,33 @@
+################################################################################
+# Big Data Analytics in Transportation (TU Dresden)
+# Master Thesis
+# This file contains all functions to execute 09_snn_calc_network_distances.R
+#############################################################################
+# Documentation: calc_nd
+# Usage: calc_nd(con, char_mapped_points, char_osm2po_subset, char_dist_mat)
+# Description: Execute query calculating the NDs between all Os and Ds
+# Args/Options: con, char_mapped_points, char_osm2po_subset, char_dist_mat
+# Returns: ...
+# Output: ...
+# Action: Executing a psql-query
+calc_nd <- function(con, char_mapped_points, char_osm2po_subset, char_dist_mat) {
+	query <- paste0("SELECT m1.id as o_m, 
+  m2.id as o_n,
+  LEAST(
+  	m1.distance_to_start + m2.distance_to_start + COALESCE(pi_pk.m, 0),
+  	m1.distance_to_end + m2.distance_to_end + COALESCE(pj_pl.m, 0),
+  	m1.distance_to_start + m2.distance_to_end + COALESCE(pi_pl.m, 0),
+  	m1.distance_to_end + m2.distance_to_start + COALESCE(pj_pk.m, 0)
+  ) AS nd
+  FROM ", char_mapped_points, " m1
+  CROSS JOIN", char_mapped_points, " m2
+  INNER JOIN ", char_osm2po_subset, " e_ij ON m1.line_id = e_ij.id
+  INNER JOIN ", char_osm2po_subset, " e_kl ON m2.line_id  = e_kl.id
+  INNER JOIN ", char_dist_mat, " pi_pk ON pi_pk.source = LEAST(e_ij.source, e_kl.source) AND pi_pk.target = GREATEST(e_ij.source, e_kl.source)
+  INNER JOIN ", char_dist_mat, " pj_pl ON pj_pl.source = LEAST(e_ij.target, e_kl.target) AND pj_pl.target = GREATEST(e_ij.target, e_kl.target)
+  INNER JOIN ", char_dist_mat, " pi_pl ON pi_pl.source = LEAST(e_ij.source, e_kl.target) AND pi_pl.target = GREATEST(e_ij.source, e_kl.target)
+  INNER JOIN ", char_dist_mat, " pj_pk ON pj_pk.source = LEAST(e_ij.target, e_kl.source) AND pj_pk.target = GREATEST(e_ij.target, e_kl.source)
+  where m1.id < m2.id;")
+	
+	dbExecute(con, query)
+}
